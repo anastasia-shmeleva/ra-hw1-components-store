3d59b792b981da4c633f90fdd2b3030d74d81156
Anastasia Shmeleva <anastasiashmeleva@Anastasias-MacBook-Air-2.local>
2021-10-30 11:49:05 +0300

    app structure fix

--- project/src/App.js
+++ project/src/App.js
@@ -1,33 +1,33 @@
 import './App.css';
 
-function App() {
-  const item = {
-    brand: 'Tiger of Sweden',
-    title: 'Leonard coat',
-    description: 'Minimalistic coat in cotton-blend',
-    descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
-    price: 399,
-    currency: '£'
-  }
-
-  function ShopItemFunc(props) {
-    const data = props.item;
-    return(
-      <div className="main-content">
-        <h2>{data.brand}</h2>
-        <h1>{data.title}</h1>
-        <h3>{data.description}</h3>
-        <div className="description">{data.descriptionFull}</div>
-        <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
-        <div className="divider"></div>
-        <div className="purchase-info">
-          <div className="price">{`${data.currency}${data.price.toFixed(2)}`}</div>
-          <button>Добавить в корзину</button>
-        </div>
+function ShopItemFunc(props) {
+  const data = props.item;
+  return(
+    <div className="main-content">
+      <h2>{data.brand}</h2>
+      <h1>{data.title}</h1>
+      <h3>{data.description}</h3>
+      <div className="description">{data.descriptionFull}</div>
+      <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+      <div className="divider"></div>
+      <div className="purchase-info">
+        <div className="price">{`${data.currency}${data.price.toFixed(2)}`}</div>
+        <button>Добавить в корзину</button>
       </div>
-    )
-  }
+    </div>
+  )
+}
 
+const item = {
+  brand: 'Tiger of Sweden',
+  title: 'Leonard coat',
+  description: 'Minimalistic coat in cotton-blend',
+  descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+  price: 399,
+  currency: '£'
+}
+
+function App() {
   return (
     <div className="container">
       <div className="background-element">
